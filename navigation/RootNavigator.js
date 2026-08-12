a53c9131c8519a1ba8afbd29659bf8123dc256ec
Abk-tech-web <abubakarmybinance@gmail.com>
2026-08-12 22:52:42 +0100

    Add files via upload

--- navigation/RootNavigator.js
+++ navigation/RootNavigator.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
+import { useAuth } from '../context/AuthContext';
+import { colors } from '../config/theme';
+import AuthNavigator from './AuthNavigator';
+import KycNavigator from './KycNavigator';
+import MainTabNavigator from './MainTabNavigator';
 
+const navTheme = {
+  ...DarkTheme,
+  colors: { ...DarkTheme.colors, background: colors.bg, card: colors.bgElevated, border: colors.border, primary: colors.violet },
+};
+
+export default function RootNavigator() {
+  const { user } = useAuth();
+
+  return (
+    <NavigationContainer theme={navTheme}>
+      {!user ? <AuthNavigator /> : user.kycStatus !== 'approved' ? <KycNavigator /> : <MainTabNavigator />}
+    </NavigationContainer>
+  );
+}
