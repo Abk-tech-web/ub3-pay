3d002bad84c6adac50d73697a15c758c3f869de9
Abk-tech-web <abubakarmybinance@gmail.com>
2026-08-12 22:19:53 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AuthProvider } from './src/context/AuthContext';
+import { WalletProvider } from './src/context/WalletContext';
+import RootNavigator from './src/navigation/RootNavigator';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <AuthProvider>
+        <WalletProvider>
+          <StatusBar style="light" />
+          <RootNavigator />
+        </WalletProvider>
+      </AuthProvider>
+    </SafeAreaProvider>
+  );
+}
